fb59a7904eaaaac55b7c48d1dd76599431fd3e44
Veehipee001 <dapoolamide87@gmail.com>
2025-06-21 06:41:30 +0100

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import os
 import telebot
 
-TOKEN = os.environ["7765489129:AAECLQL8HdmJUWaws8IfqvWcLve7WWGOCgk"]
+TOKEN = os.environ["7765489129:AAECLQL8HdmJUWaws8IfqvWcLve7WWGOCgK"]
 bot = telebot.TeleBot(TOKEN)
-print("7765489129:AAECLQL8HdmJUWaws8IfqvWcLve7WWGOCgk:", TOKEN)
